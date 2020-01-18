e5268dcd207dd5dd3a1b9bd0c1008d8aff89c88c
blacktop <blacktop@users.noreply.github.com>
2020-01-18 14:10:12 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ latest="1.0"
 if test -f LATEST; then
     latest=$(cat ./LATEST)
 elif test -f VERSION; then
-    latest=$(cat ./LATEST)
+    latest=$(cat ./VERSION)
 fi
 
 echo ::set-output name=version::$latest
